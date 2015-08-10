c2148930b2a73d74754917485f0e0318117f3019
Miguel A. Mejias <miguelangelmejias@dorna.com>
2015-08-10 08:57:54 +0200

    Print help if no args, and add check for subtrees.json file presence.

--- index.js
+++ index.js
@@ -6,19 +6,40 @@ var colors = require('colors');
 var shell = require('./lib/shell');
 var subtree = require('./lib/subtree');
 
-var cwd = process.cwd();
-var gitroot = path.join(cwd, ".git")
+var args = process.argv.slice(2);
 
-try {
-    fs.readdirSync(gitroot);
-} catch (error) {
-    console.error('Run this script from the root of the repository.'.red);
-    process.exit(1);
-}
+if (args.length > 0) {
 
-subtree.run(process.argv.slice(2), function(err, result) {
-    if (err) {
-        console.error(('ERROR: ' + err).red);
+    // Check if it's project root
+    try {
+        var gitroot = path.join(process.cwd(), ".git")
+        fs.readdirSync(gitroot);
+    } catch (error) {
+        console.error('Run this script from the root of the repository.'.red);
         process.exit(1);
     }
-});
+    
+    // Check if config file exists
+    try {
+        var subtrees = path.join(process.cwd(), "subtrees.json")
+        fs.readFileSync(subtrees);
+    } catch (error) {
+        console.error('Missing subtrees.json configuration file'.red);
+        process.exit(1);
+    }
+
+    subtree.run(args, function(err, result) {
+        if (err) {
+            console.error(('ERROR: ' + err).red);
+            process.exit(1);
+        }
+    });
+} else {
+    console.log('usage: git-subtree <command>\n');
+    console.log('Commands:');
+    console.log('init\tInitialize project subtrees');
+    console.log('add\tCreate remote, fetch and add subtree folder');
+    console.log('pull\tPull changes from subtree');
+    console.log('push\tPush changes to subtree');
+    console.log('commit\tCommit subtree folder changes');
+}
